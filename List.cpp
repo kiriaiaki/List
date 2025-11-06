63ddff3ab238b1bb83de4d6c2d3f70ffe8136693
kiriaiaki <kirkos29028@gmail.com>
2025-11-06 03:53:16 +0300

    before delete

--- List.cpp
+++ List.cpp
@@ -2,58 +2,32 @@
 
 int main ()
 {
+    FILE* file_html = fopen ("Debag.html", "w");
+    fprintf (file_html, "<pre>\n\n");
+    fprintf (file_html, "+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+\n\n");
+
     list_k List = {};
-    if (List_Ctor (&List) == There_Are_Errors)
+    if (List_Ctor (&List, file_html) == There_Are_Errors)
     {
-        printf ("!!!EEERRRRRROOORRR!!!\n");
+        printf ("\033[31m!!!EEERRRRRROOORRR!!!\033[0m\n");
         return 0;
     }
 
-    FILE* file_html = fopen ("Debag.html", "w");
-    fprintf (file_html, "<pre>\n\n");
+    List_Insert_On (10, 1, &List, file_html);
+    List_Insert_On (20, 2, &List, file_html);
+    List_Insert_On (30, 3, &List, file_html);
+    List_Insert_On (25, 3, &List, file_html);
+    List_Insert_On (5, 1, &List, file_html);
+    List_Insert_On (15, 2, &List, file_html);
+    List_Insert_On (2007, 1, &List, file_html);
 
-    List_Dump_In_Html (&List, file_html);
 
-    List.Array_Value[1] = 10;
-    List.Size = 1;
-    List.Head = 1;
-    List.Tail = 1;
-    List.Array_Next[1] = 0;
-    List.Array_Next[List.Capacity - 1] = 2;
-    List.Array_Prev[1] = 0;
-    List.Array_Next[0] = 1;
-    List.Array_Prev[0] = 1;
-    List.Free = 2;
-    List_Dump_In_Html (&List, file_html);
-
-    List.Array_Value[2] = 20;
-    List.Size = 2;
-    List.Head = 2;
-    List.Tail = 1;
-    List.Array_Next[0] = 1;
-    List.Array_Next[List.Capacity - 1] = 3;
-    List.Array_Prev[0] = 2;
-    List.Array_Next[1] = 2;
-    List.Array_Prev[1] = 0;
-    List.Array_Next[2] = 0;
-    List.Array_Prev[2] = 1;
-    List.Free = 3;
-    List_Dump_In_Html (&List, file_html);
-
-    List.Array_Next[5] = 45;
-    List_Dump_In_Html (&List, file_html);
-
-    List.Array_Prev[7] = 23;
-    List_Dump_In_Html (&List, file_html);
-
-    fprintf (file_html, "+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+\n\n");
     fclose (file_html);
-
     List_Dtor (&List);
     return 0;
 }
 
-int List_Ctor (list_k* const List)
+int List_Ctor (list_k* const List, FILE* file_html)
 {
     List->Size = 0;
     List->Capacity = 10;
@@ -65,7 +39,6 @@ int List_Ctor (list_k* const List)
     }
     List->Array_Value[0] = Canary;
 
-    List->Head = 0;
     List->Array_Next = (size_t*) calloc (List->Capacity, sizeof (size_t));
     if (List->Array_Next == NULL)
     {
@@ -77,9 +50,8 @@ int List_Ctor (list_k* const List)
     {
         List->Array_Next[i] = i + 1;
     }
-    List->Array_Next[List->Capacity - 1] = 1;
+    List->Array_Next[List->Capacity - 1] = 0;
 
-    List->Tail = 0;
     List->Array_Prev = (ssize_t*) calloc (List->Capacity, sizeof (ssize_t));
     if (List->Array_Prev == NULL)
     {
@@ -93,6 +65,9 @@ int List_Ctor (list_k* const List)
     }
 
     List->Free = 1;
+
+    fprintf (file_html, "<u>Dump after function creator</u>\n\n");
+    List_Dump_In_Html (List, file_html);
     return 0;
 }
 
@@ -101,27 +76,160 @@ int List_Dtor (list_k* const List)
     List->Size = 0;
     List->Capacity = 0;
 
-    List->Array_Value[0] = 0;
     free (List->Array_Value);
 
-    List->Head = 0;
-    for (size_t i = 0; i < List->Capacity; i++)
-    {
-        List->Array_Next[i] = 0;
-    }
     free (List->Array_Next);
 
-    List->Tail = 0;
-    for (size_t i = 0; i < List->Capacity; i++)
-    {
-        List->Array_Prev[i] = 0;
-    }
     free (List->Array_Prev);
 
     List->Free = 0;
 
     return 0;
 }
+
+int List_Error (const list_k* const List)
+{
+    if (List == NULL)
+    {
+        return Not_Struct_List;
+    }
+
+    if (List->Capacity < 10)
+    {
+        return Bad_Capacity;
+    }
+
+    if (List->Array_Value == NULL)
+    {
+        return Not_Array_Data;
+    }
+
+    if (List->Array_Next == NULL)
+    {
+        return Not_Array_Next;
+    }
+
+    if (List->Array_Prev == NULL)
+    {
+        return Not_Array_Prev;
+    }
+
+    if (List->Size >= List->Capacity)
+    {
+        return Bad_Size;
+    }
+
+    if (List->Array_Value[0] != Canary)
+    {
+        return Bad_Canary;
+    }
+
+    if (List->Free == 0 || List->Free >= List->Capacity)
+    {
+        return Bad_Free;
+    }
+
+    if (List->Array_Prev[0] >= ssize_t (List->Capacity))
+    {
+        return Bad_Head;
+    }
+
+    if (List->Array_Next[0] >= List->Capacity)
+    {
+        return Bad_Tail;
+    }
+
+    size_t Counter_Free_Element = 0;
+    for (size_t i = 0; i < List->Capacity; i++)
+    {
+        if (List->Array_Prev[i] == -1)
+        {
+            Counter_Free_Element++;
+        }
+    }
+    if (Counter_Free_Element != (List->Capacity - 1) - List->Size)
+    {
+        return Bad_Count_Free;
+    }
+
+    for (size_t i = 0; i < List->Capacity; i++)
+    {
+        if (List->Array_Prev[i] >= ssize_t (List->Capacity))
+        {
+            return Impossible_Prev;
+        }
+
+        else if (List->Array_Next[i] >= List->Capacity)
+        {
+            return Impossible_Next;
+        }
+    }
+
+    for (size_t i = 0; i < List->Capacity; i++)
+    {
+        for (size_t j = i + 1; j < List->Capacity; j++)
+        {
+            if (List->Array_Prev[i] == List->Array_Prev[j] && List->Array_Prev[j] != -1)
+            {
+                return Repeat_Prev;
+            }
+
+            else if (List->Array_Next[i] == List->Array_Next[j])
+            {
+                return Repeat_Next;
+            }
+        }
+    }
+
+    for (size_t i = 0; i < List->Capacity; i++)
+    {
+        if (ssize_t (i) != List->Array_Prev[List->Array_Next[i]])
+        {
+            if (List->Array_Prev[i] == -1 && List->Array_Prev[List->Array_Next[i]] == -1)
+            {
+                continue;
+            }
+
+            else
+            {
+                return Bad_Cycle;
+            }
+        }
+    }
+
+    size_t Len_Free_Cycle = 0;
+    for (size_t i = List->Free; List->Array_Next[i] != List->Free; i = List->Array_Next[i])
+    {
+        Len_Free_Cycle++;
+    }
+    if (Len_Free_Cycle != (List->Capacity - 1) - List->Size - 1)
+    {
+        return Bad_Len_Free_Cycle;
+    }
+
+    size_t Len_Next_Cycle = 0;
+    for (size_t i = 0; List->Array_Next[i] != 0; i = List->Array_Next[i])
+    {
+        Len_Next_Cycle++;
+    }
+    if (Len_Next_Cycle != List->Size)
+    {
+        return Bad_Len_Next_Cycle;
+    }
+
+    size_t Len_Prev_Cycle = 0;
+    for (ssize_t i = 0; List->Array_Prev[i] != 0; i = List->Array_Prev[i])
+    {
+        Len_Prev_Cycle++;
+    }
+    if (Len_Prev_Cycle != List->Size)
+    {
+        return Bad_Len_Prev_Cycle;
+    }
+
+    return Not_Error_List;
+}
+
 
 int List_Dump_In_Html (const list_k* const List, FILE* file_html)
 {
@@ -178,6 +286,18 @@ int Naming_Command_Dot (char* const Name_Command, char* const Name_File)
 
 int Dump_For_Graph (const list_k* const List, FILE* const file)
 {
+    size_t Repeat_Free = 0;
+    for (size_t i = 0; i < List->Capacity; i++)
+    {
+        for (size_t j = i + 1; j < List->Capacity; j++)
+        {
+            if (List->Array_Next[i] == List->Array_Next[j])
+            {
+                Repeat_Free = List->Array_Next[i];
+            }
+        }
+    }
+
     fprintf (file, "digraph G\n"
                    "{\n"
                    "    rankdir = LR;\n"
@@ -185,9 +305,19 @@ int Dump_For_Graph (const list_k* const List, FILE* const file)
                    "    edge[color = \"red\", style = \"bold\"];\n");
     fprintf (file, "\n");
 
-    for (size_t i = 0; i < List->Capacity; i++)
+    fprintf (file, "    %d [shape = Mrecord, label = \"index = %d | value = %d | {head = %zd | tail = %zu}\", style = \"filled\", fillcolor = \"lightblue\"];\n", 0, 0, List->Array_Value[0], List->Array_Prev[0], List->Array_Next[0]);
+
+    for (size_t i = 1; i < List->Capacity; i++)
     {
-        fprintf (file, "    %zu [shape = Mrecord, label = \"index = %zu | value = %d | {prev = %zd | next = %zu}\", style = \"filled\", fillcolor = \"lightgrey\"];\n", i, i, List->Array_Value[i], List->Array_Prev[i], List->Array_Next[i]);
+        if (List->Array_Prev[i] == -1)
+        {
+            fprintf (file, "    %zu [shape = Mrecord, label = \"index = %zu | value = %d | {prev = %zd | next = %zu}\", style = \"filled\", fillcolor = \"lightgrey\"];\n", i, i, List->Array_Value[i], List->Array_Prev[i], List->Array_Next[i]);
+        }
+
+        else
+        {
+            fprintf (file, "    %zu [shape = Mrecord, label = \"index = %zu | value = %d | {prev = %zd | next = %zu}\", style = \"filled\", fillcolor = \"lightblue\"];\n", i, i, List->Array_Value[i], List->Array_Prev[i], List->Array_Next[i]);
+        }
     }
     fprintf (file, "\n");
 
@@ -197,13 +327,13 @@ int Dump_For_Graph (const list_k* const List, FILE* const file)
     }
     fprintf (file, "\n");
 
-    fprintf (file, "    head [shape = invhouse, label = \"head = %zu\", style = \"filled\", fillcolor = \"goldenrod1\"];\n", List->Head);
-    fprintf (file, "    tail [shape = invhouse, label = \"tail = %zu\", style = \"filled\", fillcolor = \"pink2\"];\n", List->Tail);
+    fprintf (file, "    head [shape = invhouse, label = \"head = %zd\", style = \"filled\", fillcolor = \"goldenrod1\"];\n", List->Array_Prev[0]);
+    fprintf (file, "    tail [shape = invhouse, label = \"tail = %zu\", style = \"filled\", fillcolor = \"pink2\"];\n", List->Array_Next[0]);
     fprintf (file, "    free [shape = invhouse, label = \"first free = %zu\", style = \"filled\", fillcolor = \"green\"];\n", List->Free);
     fprintf (file, "\n");
 
-    fprintf (file, "    {rank = same; head; %zu};\n", List->Head);
-    fprintf (file, "    {rank = same; tail; %zu};\n", List->Tail);
+    fprintf (file, "    {rank = same; head; %zd};\n", List->Array_Prev[0]);
+    fprintf (file, "    {rank = same; tail; %zu};\n", List->Array_Next[0]);
     fprintf (file, "    {rank = same; free; %zu};\n", List->Free);
     fprintf (file, "\n");
 
@@ -213,7 +343,20 @@ int Dump_For_Graph (const list_k* const List, FILE* const file)
         {
             if (List->Array_Prev[i] == -1 && List->Array_Prev[List->Array_Next[i]] == -1)
             {
-                fprintf (file, "    %zu->%zu[color = \"green\"];\n", i, List->Array_Next[i]);
+                if (List->Array_Next[i] != Repeat_Free)
+                {
+                    fprintf (file, "    %zu->%zu[color = \"green\"];\n", i, List->Array_Next[i]);
+                }
+
+                else
+                {
+                    fprintf (file, "    %zu->%zu[label = \"Повторные стрелки\"];\n", i, List->Array_Next[i]);
+                }
+            }
+
+            else if (List->Array_Prev[i] == -1 && List->Array_Next[i] == 0)
+            {
+                continue;
             }
 
             else
@@ -225,13 +368,14 @@ int Dump_For_Graph (const list_k* const List, FILE* const file)
 
                 else
                 {
-                    fprintf (file, "    %zu->%zu[color = \"pink2\"];\n", i, List->Array_Next[i]);
+                    fprintf (file, "    %zu->%zu[label = \"Неправильный цикл\"];\n", i, List->Array_Next[i]);
                 }
             }
         }
+
         else
         {
-            fprintf (file, "    %zu->%zu;\n", i, List->Array_Next[i]);
+            fprintf (file, "    %zu->%zu[label = \"Невозможное значение\"];\n", i, List->Array_Next[i]);
         }
     }
     fprintf (file, "\n");
@@ -244,12 +388,12 @@ int Dump_For_Graph (const list_k* const List, FILE* const file)
             {
                 if (List->Array_Next[List->Array_Prev[i]] != i)
                 {
-                    fprintf (file, "    %zu->%zd[color = \"goldenrod1\"];\n", i, List->Array_Prev[i]);
+                    fprintf (file, "    %zu->%zd[label = \"Неправильный цикл\"];\n", i, List->Array_Prev[i]);
                 }
             }
             else
             {
-                fprintf (file, "    %zu->%zd;\n", i, List->Array_Prev[i]);
+                fprintf (file, "    %zu->%zd[label = \"Невозможное значение\"];\n", i, List->Array_Prev[i]);
             }
         }
     }
@@ -260,11 +404,16 @@ int Dump_For_Graph (const list_k* const List, FILE* const file)
 
 int Dump_For_Html (const list_k* const List, FILE* const file, const char* const Name_File)
 {
-    fprintf (file, "+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+\n\n");
+    int Number_Error = List_Error (List);
+    if (Number_Error != 0)
+    {
+        fprintf (file, "                          <b>%s</b>\n", Array_Name_List_Error[Number_Error]);
+    }
+
     fprintf (file, "SIZE = %zu\n", List->Size);
     fprintf (file, "CAPACITY = %zu\n", List->Capacity);
-    fprintf (file, "HEAD = %zu\n", List->Head);
-    fprintf (file, "TAIL = %zu\n", List->Tail);
+    fprintf (file, "HEAD = %zd\n", List->Array_Prev[0]);
+    fprintf (file, "TAIL = %zu\n", List->Array_Next[0]);
     fprintf (file, "FREE = %zu\n", List->Free);
     fprintf (file, "\n");
 
@@ -380,6 +529,34 @@ int Dump_For_Html (const list_k* const List, FILE* const file, const char* const
     fprintf (file, "<img src = \"%s\">\n", Name_File);
     fprintf (file, "\n");
 
+    fprintf (file, "+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+\n\n");
+
+    return 0;
+}
+
+
+int List_Insert_On (const int Value, const int Index, list_k* const List, FILE* file_html)
+{
+    if (Index <= 0 || Index >= int (List->Capacity) || List->Array_Prev[Index - 1] == -1)
+    {
+        printf ("Wrong insert (%d)", Index);
+        return There_Are_Errors;
+    }
+
+    size_t Insert_Position = List->Free;
+    List->Free = List->Array_Next[List->Free];
+
+    List->Array_Value[Insert_Position] = Value;
+    List->Size++;
+
+    List->Array_Next[Insert_Position] = List->Array_Next[Index - 1];
+    List->Array_Next[Index - 1] = Insert_Position;
+
+    List->Array_Prev[Insert_Position] = Index - 1;
+    List->Array_Prev[List->Array_Next[Insert_Position]] = ssize_t (Insert_Position);
+
+    fprintf (file_html, "<u>Dump after function insert on (%d, %d)</u>\n\n", Value, Index);
+    List_Dump_In_Html (List, file_html);
     return 0;
 }
 
--- List.h
+++ List.h
@@ -9,22 +9,71 @@ struct list_k
     int* Array_Value;
     size_t* Array_Next;
     ssize_t* Array_Prev;
-    size_t Head;
-    size_t Tail;
     size_t Free;
+};
+
+enum Name_List_Error
+{
+    Not_Error_List     =  0,
+    Not_Struct_List    =  1,
+    Bad_Capacity       =  2,
+    Not_Array_Data     =  3,
+    Not_Array_Next     =  4,
+    Not_Array_Prev     =  5,
+    Bad_Size           =  6,
+    Bad_Canary         =  7,
+    Bad_Free           =  8,
+    Bad_Head           =  9,
+    Bad_Tail           = 10,
+    Bad_Count_Free     = 11,
+    Impossible_Prev    = 12,
+    Impossible_Next    = 13,
+    Repeat_Prev        = 14,
+    Repeat_Next        = 15,
+    Bad_Cycle          = 16,
+    Bad_Len_Free_Cycle = 17,
+    Bad_Len_Next_Cycle = 18,
+    Bad_Len_Prev_Cycle = 19,
+    Quantity_Error     = 20
+};
+
+const char* Array_Name_List_Error[Quantity_Error] = {
+    "NOT ERROR LIST",
+    "!!!NOT STRUCT LIST!!!",
+    "!!!BAD CAPACITY!!!",
+    "!!!NOT ARRAY DATA!!!",
+    "!!!NOT ARRAY NEXT!!!",
+    "!!!NOT ARRAY PREV!!!",
+    "!!!BAD SIZE!!!",
+    "!!!BAD CANARY!!!",
+    "!!!BAD FREE!!!",
+    "!!!BAD HEAD!!!",
+    "!!!BAD TAIL!!!",
+    "!!!BAD COUNT FREE!!!",
+    "!!!IMPOSSIBLE PREV!!!",
+    "!!!IMPOSSIBLE NEXT!!!",
+    "!!!REPEAT PREV!!!",
+    "!!!REPEAT NEXT!!!",
+    "!!!BAD CYCLE!!!",
+    "!!!BAD LEN FREE CYCLE!!!",
+    "!!!BAD LEN NEXT CYCLE!!!",
+    "!!!BAD LEN PREV CYCLE!!!"
 };
 
 const int There_Are_Errors = -2902;
 const int Canary = 2008;
 
-int List_Ctor (list_k* const List);
-int List_Dtor (list_k* const List);
+int List_Ctor  (list_k* const List, FILE* file_html);
+int List_Dtor  (list_k* const List);
+int List_Error (const list_k* const List);
 
-int List_Dump_In_Html (const list_k* const List, FILE* file_html);
-int Naming_File (char* const Name_File);
+int List_Dump_In_Html  (const list_k* const List, FILE* file_html);
+int Naming_File        (char* const Name_File);
 int Naming_Command_Dot (char* const Name_Command, char* const Name_File);
-int Dump_For_Graph (const list_k* const List, FILE* const file);
-int Dump_For_Html (const list_k* const List, FILE* const file, const char* const Name_File);
+int Dump_For_Graph     (const list_k* const List, FILE* const file);
+int Dump_For_Html      (const list_k* const List, FILE* const file, const char* const Name_File);
+
+int List_Insert_On (const int Value, const int Index, list_k* const List, FILE* file_html);
 
 int Reverse_Str (char* const Str);
 char* itoa_k    (int Number, char* const Str);
